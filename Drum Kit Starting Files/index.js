480828537a46ba4cf76aa890f6818b32a6932785
Shubhi6835 <shubhr68350@gmail.com>
2025-04-26 14:25:35 +0530

    added animation

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -1,36 +1,51 @@
 document.querySelectorAll(".drum")[0].addEventListener("click", function handleEvent(){
-    this.style.color="white";
     var audio = new Audio("./sounds/tom-1.mp3");
     audio.play();
-  });
+    var buttonInnerHTML= this.innerHTML;
+    buttonPress(buttonInnerHTML); 
+    });
 document.querySelectorAll(".drum")[1].addEventListener("click", function handleEvent(){
     var audio = new Audio("./sounds/tom-2.mp3");
     audio.play();
+    var buttonInnerHTML= this.innerHTML;
+    buttonPress(buttonInnerHTML);
   });
 document.querySelectorAll(".drum")[2].addEventListener("click", function handleEvent(){
     var audio = new Audio("./sounds/tom-3.mp3");
     audio.play();
+    var buttonInnerHTML= this.innerHTML;
+    buttonPress(buttonInnerHTML);
   });
 document.querySelectorAll(".drum")[3].addEventListener("click", function handleEvent(){
     var audio = new Audio("./sounds/tom-4.mp3");
     audio.play();
+    var buttonInnerHTML= this.innerHTML;
+    buttonPress(buttonInnerHTML);
   });
 document.querySelectorAll(".drum")[4].addEventListener("click", function handleEvent(){
     var audio = new Audio("./sounds/crash.mp3");
     audio.play();
+    var buttonInnerHTML= this.innerHTML;
+    buttonPress(buttonInnerHTML);
   });
 document.querySelectorAll(".drum")[5].addEventListener("click", function handleEvent(){
     var audio = new Audio("./sounds/kick-bass.mp3");
     audio.play();
+    var buttonInnerHTML= this.innerHTML;
+    buttonPress(buttonInnerHTML);
   });
 document.querySelectorAll(".drum")[6].addEventListener("click", function handleEvent(){
     var audio = new Audio("./sounds/snare.mp3");
     audio.play();
+    var buttonInnerHTML= this.innerHTML;
+    buttonPress(buttonInnerHTML);
   });
 
   document.addEventListener("keypress",function(event) 
   {
      sound(event.key);
+     var buttonInnerHTML= event.key;
+     buttonPress(buttonInnerHTML);
   });
 
   function sound(instrument){
@@ -66,4 +81,12 @@ document.querySelectorAll(".drum")[6].addEventListener("click", function handleE
       default:
         console.log("Please select the correct keyword");
   }
+  }
+  function buttonPress(buttonInnerHTML){
+     var classOfButton = document.querySelector("." + buttonInnerHTML)
+     console.log(classOfButton);
+    classOfButton.classList.add("pressed"); 
+    setTimeout(function() {
+      classOfButton.classList.remove("pressed");
+    }, 100);
   }
